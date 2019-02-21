ec1916f549c5c27e078d1f27d01bbcf31ae9a690
harvanshhpsoftech <harvansh@hpsoftech.com>
2019-02-21 18:54:10 +0530

    create Model -a

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/Products','ApiAuthController@Products');
+//Route::post('/Products','ApiAuthController@Products');
+
+Route::apiResource('/products','ProductController');
